181acbba918d94ab9b52b98f9b97bfad68998c4f
baudoin <baudoinvicbolingo@gmail.com>
2024-09-15 22:31:49 +0200

    today commit

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,27 +4,24 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
+    host: "smtp.gmail.com",
     port: "587",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "baudoinvicbolingo@gmail.com",
+      pass: "jpgrzjhdijbjllhp",
     },
     tls: {
       rejectUnauthorized: false,
     },
   });
 
-
-  
-  const options = {
-    from: `"${send_to}" <${process.env.EMAIL_USER}>`,
-    to: process.env.EMAIL_USER,
-    replyTo: reply_to,
-    subject: subject,
-    html: message,
-  };
-
+   const options = {
+     from: sent_from,
+     to: send_to,
+     replyTo: reply_to,
+     subject: subject,
+     html: message,
+   };
 
 
   transporter.sendMail(options, function (err, info) {
@@ -37,3 +34,6 @@ const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
 };
 
 module.exports = sendEmail;
+
+
+
